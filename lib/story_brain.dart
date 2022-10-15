0452da5689674a70f1327edd8a6488674f7cf980
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-10-15 16:20:00 +0200

    11. Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -45,12 +45,14 @@ class StoryBrain {
 
   // COMPLETED: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
   String get story => _storyData.first.storyTitle;
+
+  // COMPLETED: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
+  String get choice1 => _storyData.first.choice1;
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 
-//TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
 
 //TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
 
