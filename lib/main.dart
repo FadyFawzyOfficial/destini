1bb7a260ee10ade262eb2b932f77f9b2a295c6ef
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-10-15 16:22:10 +0200

    13. Step 13 - Use the storyBrain to get the text for choice 1

--- lib/main.dart
+++ lib/main.dart
@@ -64,10 +64,10 @@ class StoryPageState extends State<StoryPage> {
                     // TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
                   },
                   style: TextButton.styleFrom(backgroundColor: Colors.red),
-                  child: const Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  child: Text(
+                    // COMPLETED: Step 13 - Use the storyBrain to get the text for choice 1.
+                    storyBrain.choice1,
+                    style: const TextStyle(color: Colors.white, fontSize: 20),
                   ),
                 ),
               ),
