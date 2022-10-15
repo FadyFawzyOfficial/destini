8ecff9ae4e15e98bf836606ec2f20fb4ba9fcc0f
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-10-15 17:12:59 +0200

    24. Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class StoryPageState extends State<StoryPage> {
                   onPressed: () {
                     // Choice 1 made by user.
                     // COMPLETED: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
-                    storyBrain.nextStory(1);
+                    setState(() => storyBrain.nextStory(1));
                   },
                   style: TextButton.styleFrom(backgroundColor: Colors.red),
                   child: Text(
@@ -82,7 +82,7 @@ class StoryPageState extends State<StoryPage> {
                   onPressed: () {
                     //Choice 2 made by user.
                     // COMPLETED: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
-                    storyBrain.nextStory(2);
+                    setState(() => storyBrain.nextStory(2));
                   },
                   style: TextButton.styleFrom(backgroundColor: Colors.blue),
                   child: Text(
@@ -100,6 +100,6 @@ class StoryPageState extends State<StoryPage> {
   }
 }
 
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
+// COMPLETED: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
 
 //TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
