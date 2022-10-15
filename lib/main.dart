20bb875c33a042f3b1cd7bae5a66f070aa80dd0e
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-10-15 16:09:02 +0200

    9. Step 9 - Create a new storyBrain object from the StoryBrain class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'story_brain.dart';
+
 //TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
 
 void main() => runApp(const Destini());
@@ -16,7 +18,8 @@ class Destini extends StatelessWidget {
   }
 }
 
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
+// COMPLETED: Step 9 - Create a new storyBrain object from the StoryBrain class.
+StoryBrain storyBrain = StoryBrain();
 
 class StoryPage extends StatefulWidget {
   const StoryPage({super.key});
